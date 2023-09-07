5222da489b0ec9b17c39643de88166f0d33a3802
Yaroslav Barkovskiy <y.barkovskij@unact.ru>
2023-09-07 15:08:13 +0300

    DEV-42127

--- lib/widgets/entity_image.dart
+++ lib/widgets/entity_image.dart
@@ -4,17 +4,21 @@ class EntityImage extends StatelessWidget {
   final String imageUrl;
   final String imagePath;
   final bool local;
+  final Color color;
+  final void Function()? onTap;
 
   EntityImage({
     Key? key,
     required this.imageUrl,
     required this.imagePath,
-    required this.local
+    required this.local,
+    this.color = Colors.blue,
+    this.onTap
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    if (!local) return RetryableImage(imageUrl: imageUrl);
+    if (!local) return RetryableImage(imageUrl: imageUrl, color: color, onTap: onTap);
 
     return FutureBuilder(
       future: getApplicationDocumentsDirectory(),
@@ -25,7 +29,13 @@ class EntityImage extends StatelessWidget {
 
         if (!file.existsSync()) return const SizedBox(width: 0, height: 0);
 
-        return Image.file(file, errorBuilder: (context, error, stackTrace) => const Icon(Icons.error));
+        return GestureDetector(
+          onTap: onTap,
+          child: Image.file(
+            file,
+            errorBuilder: (context, error, stackTrace) => Icon(Icons.error, color: color)
+          ),
+        );
       }
     );
   }
