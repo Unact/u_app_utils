79ada7a60afd32374bdd097cd44fedcba79c731f
Yaroslav Barkovskiy <y.barkovskij@unact.ru>
2025-08-22 15:45:28 +0300

    DEV-43742

--- lib/utils/initialization.dart
+++ lib/utils/initialization.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
+import 'dart:io';
 
+import 'package:flutter_datawedge/flutter_datawedge.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 
 class Initialization {
@@ -20,6 +22,30 @@ class Initialization {
         };
       },
       appRunner: appRunner
+    );
+  }
+
+  static Future<void> initializeDataWedge({required String appName, bool audioFeedback = false }) async {
+    if (!Platform.isAndroid) return;
+
+    FlutterDataWedge dw = FlutterDataWedge();
+
+    await dw.initialize();
+    await dw.createDefaultProfile(profileName: appName);
+    await dw.updateProfile(
+      profileName: appName,
+      pluginName: 'BARCODE',
+      config: {
+        'scanner_selection': 'auto',
+        'decoder_i2of5': true,
+        'decoder_qrcode': true,
+        'decoder_code128': true,
+        'decoder_ean8': true,
+        'decoder_ean13': true,
+        'decoder_datamatrix': true,
+        'decoder_gs1_datamatrix': true,
+        'decode_audio_feedback_uri': audioFeedback ? 'decode-short' : ''
+      }
     );
   }
 }
--- lib/widgets/scan_view.dart
+++ lib/widgets/scan_view.dart
@@ -1,17 +1,18 @@
 import 'dart:async';
+import 'dart:io';
 
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:soundpool/soundpool.dart';
 import 'package:vibration/vibration.dart';
-
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:flutter_datawedge/flutter_datawedge.dart' as dw;
 
 class ScanView extends StatefulWidget {
   final List<Widget> actions;
   final Widget child;
-  final bool barcodeMode;
-  final bool initScanner;
   final bool paused;
   final Function(String) onRead;
 
@@ -19,8 +20,6 @@ class ScanView extends StatefulWidget {
     required this.child,
     this.actions = const [],
     this.paused = false,
-    this.initScanner = false,
-    this.barcodeMode = false,
     required this.onRead,
     super.key
   });
@@ -29,7 +28,8 @@ class ScanView extends StatefulWidget {
   ScanViewState createState() => ScanViewState();
 }
 
-class ScanViewState extends State<ScanView> with WidgetsBindingObserver {
+class ScanViewState extends State<ScanView> {
+  static final String _kBTScannerPrefix = 'SR5600';
   final GlobalKey _qrKey = GlobalKey();
   final MobileScannerController _controller = MobileScannerController(
     cameraResolution: Size(1920, 1080),
@@ -45,55 +45,11 @@ class ScanViewState extends State<ScanView> with WidgetsBindingObserver {
     ]
   );
   String lastScan = '';
-  bool _editingFinished = false;
-  final BarcodeScannerFieldFocusNode barcodeScannerFocusNode = BarcodeScannerFieldFocusNode();
-  final TextEditingController _textEditingController = TextEditingController();
-  static final List<PhysicalKeyboardKey> _finishKeys = [
-    PhysicalKeyboardKey.enter,
-    PhysicalKeyboardKey.gameButtonLeft1
-  ];
-  static final Map<PhysicalKeyboardKey, String> _keyCodeMap = {
-    PhysicalKeyboardKey.digit0: '0',
-    PhysicalKeyboardKey.digit1: '1',
-    PhysicalKeyboardKey.digit2: '2',
-    PhysicalKeyboardKey.digit3: '3',
-    PhysicalKeyboardKey.digit4: '4',
-    PhysicalKeyboardKey.digit5: '5',
-    PhysicalKeyboardKey.digit6: '6',
-    PhysicalKeyboardKey.digit7: '7',
-    PhysicalKeyboardKey.digit8: '8',
-    PhysicalKeyboardKey.digit9: '9',
-    PhysicalKeyboardKey.period: '.',
-    PhysicalKeyboardKey.semicolon: ':',
-    PhysicalKeyboardKey.minus: '-',
-    PhysicalKeyboardKey.space: ' ',
-    PhysicalKeyboardKey.keyA: 'A',
-    PhysicalKeyboardKey.keyB: 'B',
-    PhysicalKeyboardKey.keyC: 'C',
-    PhysicalKeyboardKey.keyD: 'D',
-    PhysicalKeyboardKey.keyE: 'E',
-    PhysicalKeyboardKey.keyF: 'F',
-    PhysicalKeyboardKey.keyG: 'G',
-    PhysicalKeyboardKey.keyH: 'H',
-    PhysicalKeyboardKey.keyI: 'I',
-    PhysicalKeyboardKey.keyJ: 'J',
-    PhysicalKeyboardKey.keyK: 'K',
-    PhysicalKeyboardKey.keyL: 'L',
-    PhysicalKeyboardKey.keyM: 'M',
-    PhysicalKeyboardKey.keyN: 'N',
-    PhysicalKeyboardKey.keyO: 'O',
-    PhysicalKeyboardKey.keyP: 'P',
-    PhysicalKeyboardKey.keyQ: 'Q',
-    PhysicalKeyboardKey.keyR: 'R',
-    PhysicalKeyboardKey.keyS: 'S',
-    PhysicalKeyboardKey.keyT: 'T',
-    PhysicalKeyboardKey.keyU: 'U',
-    PhysicalKeyboardKey.keyV: 'V',
-    PhysicalKeyboardKey.keyW: 'W',
-    PhysicalKeyboardKey.keyX: 'X',
-    PhysicalKeyboardKey.keyY: 'Y',
-    PhysicalKeyboardKey.keyZ: 'Z'
-  };
+
+  StreamSubscription? dataWedgeScanSubscription;
+  StreamSubscription? mobileScannerScanSubscription;
+  StreamSubscription? onBLEScanSubscription;
+  StreamSubscription? onBLEStateSubscription;
 
   static final Soundpool _kPool = Soundpool.fromOptions(options: const SoundpoolOptions());
   static final Future<int> _kBeepId = rootBundle
@@ -112,185 +68,134 @@ class ScanViewState extends State<ScanView> with WidgetsBindingObserver {
   void initState() {
     super.initState();
 
-    WidgetsBinding.instance.addObserver(this);
+    setupMobileScanner();
+    setupDataWedgeScanner();
+    setupBLEScanner();
+  }
+
+  void setupMobileScanner() {
+    mobileScannerScanSubscription = _controller.barcodes.listen((res) {
+      Barcode? barcode = res.barcodes.firstOrNull;
+      String currentScan = barcode?.rawValue ?? '';
+
+      if (barcode == null || barcode.format == BarcodeFormat.unknown) return;
+
+      scanCode(currentScan);
+    });
+  }
+
+  void setupDataWedgeScanner() {
+    if (!Platform.isAndroid) return;
+
+    dataWedgeScanSubscription = dw.FlutterDataWedge().onScanResult.listen((res) => scanCode(res.data));
+  }
+
+  Future<void> setupBLEScanner() async {
+    if (await FlutterBluePlus.isSupported == false) return;
+
+    onBLEStateSubscription = FlutterBluePlus.adapterState.listen((BluetoothAdapterState state) async {
+      if (state != BluetoothAdapterState.on) return;
+
+      BluetoothDevice? btScanner = FlutterBluePlus.connectedDevices
+        .firstWhereOrNull((device) => device.platformName.contains(_kBTScannerPrefix));
+
+      if (btScanner == null) {
+        await FlutterBluePlus.startScan(withKeywords:[_kBTScannerPrefix], timeout: Duration(seconds: 2));
+        await FlutterBluePlus.isScanning.where((val) => val == false).first;
+
+        btScanner = (await FlutterBluePlus.scanResults.first)
+          .firstWhereOrNull((res) => res.device.platformName.contains(_kBTScannerPrefix))?.device;
+
+        if (btScanner == null) return;
+      }
+
+      if (!btScanner.isConnected) await btScanner.connect();
+
+      final dataCharacteristic = (await btScanner.discoverServices())
+        .firstWhereOrNull((service) => service.serviceUuid == Guid('fff0'))
+        ?.characteristics
+        .firstWhereOrNull((char) => char.characteristicUuid == Guid('fff1'));
+
+      if (dataCharacteristic == null) return;
+
+      await dataCharacteristic.setNotifyValue(true);
+
+      onBLEScanSubscription = dataCharacteristic.onValueReceived.listen((value) {
+        final data = String.fromCharCodes(value).replaceAll('\n\r', '');
+        final code = data.substring(1);
+
+        scanCode(code);
+      });
+    });
+  }
+
+  Future<void> scanCode(String scanData) async {
+    if (widget.paused) return;
+    if (scanData == lastScan) return;
+
+    lastScan = scanData;
+
+    _beep();
+    _vibrate();
+    widget.onRead(lastScan);
   }
 
   @override
   void dispose() {
     _controller.dispose();
-    WidgetsBinding.instance.removeObserver(this);
+    mobileScannerScanSubscription?.cancel();
+    dataWedgeScanSubscription?.cancel();
+    onBLEScanSubscription?.cancel();
+    onBLEStateSubscription?.cancel();
 
     super.dispose();
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    super.didChangeAppLifecycleState(state);
-
-    switch (state) {
-      case AppLifecycleState.detached:
-      case AppLifecycleState.hidden:
-      case AppLifecycleState.paused:
-        barcodeScannerFocusNode.unfocus();
-        return;
-      case AppLifecycleState.resumed:
-        unawaited(_controller.start());
-        barcodeScannerFocusNode.requestFocus();
-      case AppLifecycleState.inactive:
-        unawaited(_controller.stop());
-    }
-  }
-
-  String? translateChar(KeyEvent rawKeyEvent) {
-    return _keyCodeMap[rawKeyEvent.physicalKey];
-  }
-
-  void _onEditingFinished() {
-    if (widget.paused) return;
-    if (!_editingFinished) return;
-    if (_textEditingController.text == '') return;
-
-    widget.onRead(_textEditingController.text);
-    _editingFinished = false;
-    _textEditingController.text = '';
   }
 
   @override
   Widget build(BuildContext context) {
-    final double width = widget.barcodeMode ? 300 : 200;
-    final double height = widget.barcodeMode ? 150 : 200;
     final Rect scanWindow = Rect.fromCenter(
       center: MediaQuery.sizeOf(context).center(const Offset(0, -100)),
-      width: width,
-      height: height,
+      width: 300,
+      height: 150,
     );
 
-    return Focus(
-      autofocus: false,
-      onKeyEvent: (FocusNode focusNode, KeyEvent rawKeyEvent) {
-        if (rawKeyEvent is! KeyUpEvent) {
-          _textEditingController.text = _textEditingController.text + (translateChar(rawKeyEvent) ?? '');
-        }
-
-        if (!_finishKeys.contains(rawKeyEvent.physicalKey)) return KeyEventResult.ignored;
-
-        _editingFinished = true;
-        _onEditingFinished();
-
-        return KeyEventResult.ignored;
-      },
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.black,
+      appBar: AppBar(
         backgroundColor: Colors.black,
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          actions: widget.actions
-        ),
-        extendBodyBehindAppBar: false,
-        body: Stack(
-          fit: StackFit.expand,
-          children: [
-            Center(
-              child: _BarcodeScannerField(
-                focusNode: barcodeScannerFocusNode,
-                keyboardType: widget.initScanner ? null : TextInputType.none,
-                controller: _textEditingController,
-                onChanged: (String changed) => _onEditingFinished()
-              )
-            ),
-            MobileScanner(
-              key: _qrKey,
-              controller: _controller,
-              scanWindow: scanWindow,
-              onDetect: (BarcodeCapture capture) {
-                Barcode? barcode = capture.barcodes.firstOrNull;
-                String currentScan = barcode?.rawValue ?? '';
-
-                if (widget.paused) return;
-                if (barcode == null || barcode.format == BarcodeFormat.unknown) return;
-                if (currentScan == lastScan) return;
-
-                lastScan = currentScan;
-
-                _beep();
-                _vibrate();
-                widget.onRead(lastScan);
-              },
-              errorBuilder: (context, error) {
-                return Text(error.errorDetails?.message ?? '');
-              },
-              placeholderBuilder: (context) {
-                return Container(color: Colors.transparent);
-              }
-            ),
-            ScanWindowOverlay(
-              scanWindow: scanWindow,
-              controller: _controller,
-              borderRadius: BorderRadius.all(Radius.circular(10)),
-              borderWidth: 2
-            ),
-            Container(
-              padding: const EdgeInsets.only(top: 32),
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: widget.child
-              )
+        actions: widget.actions
+      ),
+      extendBodyBehindAppBar: false,
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          MobileScanner(
+            key: _qrKey,
+            controller: _controller,
+            scanWindow: scanWindow,
+            errorBuilder: (context, error) {
+              return Text(error.errorDetails?.message ?? '');
+            },
+            placeholderBuilder: (context) {
+              return Container(color: Colors.transparent);
+            }
+          ),
+          ScanWindowOverlay(
+            scanWindow: scanWindow,
+            controller: _controller,
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            borderWidth: 2
+          ),
+          Container(
+            padding: const EdgeInsets.only(top: 32),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: widget.child
             )
-          ]
-        )
+          )
+        ]
       )
     );
-  }
-}
-
-class _BarcodeScannerField extends EditableText {
-  _BarcodeScannerField({
-    FocusNode? focusNode,
-    super.keyboardType,
-    required super.controller,
-    required void Function(String) onChanged
-  }) : super(
-    autofocus: true,
-    showCursor: false,
-    onChanged: onChanged,
-    focusNode: focusNode ?? BarcodeScannerFieldFocusNode(),
-    style: const TextStyle(color: Colors.transparent),
-    cursorColor: Colors.transparent,
-    backgroundCursorColor: Colors.transparent
-  );
-
-  @override
-  _BarcodeScannerFieldState createState() => _BarcodeScannerFieldState();
-}
-
-class _BarcodeScannerFieldState extends EditableTextState {
-  @override
-  void initState() {
-    widget.focusNode.addListener(funcionListener);
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    widget.focusNode.removeListener(funcionListener);
-    super.dispose();
-  }
-
-  @override
-  void requestKeyboard() {
-    super.requestKeyboard();
-
-    SystemChannels.textInput.invokeMethod('TextInput.hide');
-  }
-
-  void funcionListener() {
-    if (widget.focusNode.hasFocus) requestKeyboard();
-  }
-}
-
-class BarcodeScannerFieldFocusNode extends FocusNode {
-  @override
-  bool consumeKeyboardToken() {
-    return false;
   }
 }
